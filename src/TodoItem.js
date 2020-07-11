8ba0bc6df449e292c92167e9b2edb1466eb81817
hejianxin <hejianxinsir@gmail.com>
2020-07-11 11:25:44 +0800

    修改样式

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
+import './TodoItem.css'
 
 export default class TodoItem extends Component{
   render(){
     return (
-      <div className="todoItem">
+      <li className="todoItem">
         {this.props.todo.title}
-      </div>
+      </li>
     )
   }
 }
